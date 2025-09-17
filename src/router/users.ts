a83cfa47a36a0a70769db813aac72a88c3bde079
coralie <russicoralie812@gmail.com>
2025-09-17 16:58:17 +0200

    add routes users

--- src/router/users.ts
+++ src/router/users.ts
@@ -5,3 +5,21 @@ export const userRouter = Router();
 
 const prisma = new PrismaClient();
 
+userRouter.get('/:userid/photos', async (req: Request, res: Response) => {
+  const userId = req.params.userid;
+  const param = await prisma.user.findUnique({
+    where: { id: parseInt(userId) },
+    select: { galerie: true }
+  });
+  res.json(param)
+})
+
+userRouter.post('/:userid/photos', async (req: Request, res: Response) => {
+  const userId = req.params.userid;
+  const { galerie }: { galerie: string[] } = req.body;
+  const param = await prisma.user.update({
+    where: { id: parseInt(userId) },
+    data: { galerie }
+  });
+  res.json(param)
+})
