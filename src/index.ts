779e7b5eaa9dc20db7dca30e5b8d29a77f4b56a8
coralie <russicoralie812@gmail.com>
2025-09-18 13:50:33 +0200

    ajout routes rdv

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { userRouter } from './router/users';
 import { authRouter } from './router/auth';
 import { voyagesRouter } from './router/voyages';
+import { rdvRouter } from './router/rdv';
 import cors from 'cors';
 import 'dotenv/config'
 
@@ -14,6 +15,7 @@ const apiRouter = Router();
 apiRouter.use('/users', userRouter);
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/voyages', voyagesRouter);
+apiRouter.use('/rdv', rdvRouter);
 
 app.use('/api', apiRouter);
 
