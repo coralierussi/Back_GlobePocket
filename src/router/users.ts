62d81ddff84b906fa11e91a47f67afd61a870bf6
coralie <russicoralie812@gmail.com>
2025-09-19 16:07:37 +0200

    Add update user

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,15 +1,85 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Sexe } from '@prisma/client';
 import { checkToken } from '../middlewares/checkToken';
 export const userRouter = express.Router();
 
 const prisma = new PrismaClient();
 
+// View all users
 userRouter.get("/", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
   console.log("getting users");
   res.json(users);
 });
+
+// Update user info
+userRouter.put('/', checkToken, async (req: Request, res: Response) => {
+  const userId = req.userId;
+  const {
+          name,
+          email,
+          mdp,
+          pseudo,
+          ville,
+          pays,
+          date_anniversaire,
+          sexe,
+          galerie,
+          documents,
+          description,
+          cp,
+          compte_bancaire
+        }: {
+          name: string;
+          email: string;
+          mdp: string;
+          pseudo: string;
+          ville: string;
+          pays: string;
+          date_anniversaire: Date;
+          sexe: Sexe;
+          galerie: string[];
+          documents: string[];
+          description: string;
+          cp: number;
+          compte_bancaire: number;
+        } = req.body;
+  const param = await prisma.user.update({
+    where: { id: userId },
+    select: { id: true, 
+      name: true, 
+      email: true, 
+      mdp: true, 
+      pseudo: true, 
+      ville: true, 
+      pays: true, 
+      date_anniversaire: true, 
+      sexe: true, 
+      galerie: true, 
+      documents: true, 
+      description: true, 
+      cp: true, 
+      compte_bancaire: true 
+    },
+    data: {
+      name,
+      email,
+      mdp,
+      pseudo,
+      ville,
+      pays,
+      date_anniversaire,
+      sexe,
+      galerie,
+      documents,
+      description,
+      cp,
+      compte_bancaire
+    }
+  });
+  res.json(param)
+});
+
 
 userRouter.get('/:userid/photos', async (req: Request, res: Response) => {
   const userId = req.params.userid;
