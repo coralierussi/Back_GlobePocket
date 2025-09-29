7f0e4d3073be379ea8afc1a0faed95cd52a64195
coralie <russicoralie812@gmail.com>
2025-09-29 12:27:55 +0200

    better manage cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: '*'
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 }));
 
 const apiRouter = Router();
